82c4e5ba14cf85888591230eeed64885131cfe1f
Ali Akbar Alizada <akbar.alizadeh5@gmail.com>
2017-03-11 11:32:08 -0800

    hahasd

--- config/application.rb
+++ config/application.rb
@@ -5,11 +5,11 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-config.assets.precompile += Ckeditor.assets
 
 module Blogadmin
   class Application < Rails::Application
 
+    config.assets.precompile += Ckeditor.assets
     # Settings in config/environments/* take precedence over those specified here.
     config.assets.initialize_on_precompile = false
     # Application configuration should go into files in config/initializers
