e6c9a917ba0d838d83410a82b6445702a5b8b875
Ali Akbar Alizada <akbar.alizadeh5@gmail.com>
2017-03-11 01:32:55 -0800

    precompile

--- app/uploaders/ckeditor_picture_uploader.rb
+++ app/uploaders/ckeditor_picture_uploader.rb
@@ -33,7 +33,7 @@ class CkeditorPictureUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-    process :resize_to_fill => [118, 100]
+    process :resize_to_fill => [120, 120]
   end
 
   version :content do
