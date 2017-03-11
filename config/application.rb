0f6178448b8eacc03c9e7b0ab226ea9131adaf32
Ali Akbar Alizada <akbar.alizadeh5@gmail.com>
2017-03-11 11:40:08 -0800

    hahasdsdasfa

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ module Blogadmin
     config.assets.precompile += %w( ckeditor/* )
     config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
     # Settings in config/environments/* take precedence over those specified here.
-    config.assets.initialize_on_precompile = false
+    # config.assets.initialize_on_precompile = false
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
   end
