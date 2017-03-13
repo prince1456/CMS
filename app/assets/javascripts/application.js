af35484e0e67fe572626723283063c83218b6f00
Ali Akbar Alizada <akbar.alizadeh5@gmail.com>
2017-03-12 23:21:28 -0700

    make side bar sticky

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,26 @@
 //= require ckeditor/init
 //= require turbolinks
 //= require_tree .
+
+
+
+$(function() {
+
+    var $sidebar   = $("#sidebar"),
+        $window    = $(window),
+        offset     = $sidebar.offset(),
+        topPadding = 60;
+
+    $window.scroll(function() {
+        if ($window.scrollTop() > offset.top) {
+            $sidebar.stop().animate({
+                marginTop: $window.scrollTop() - offset.top + topPadding
+            });
+        } else {
+            $sidebar.stop().animate({
+                marginTop: 0
+            });
+        }
+    });
+
+});
