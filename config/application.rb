f8183a17145a5835b9b2adf3aca2a33fcc65a502
Ali Akbar Alizada <akbar.alizadeh5@gmail.com>
2017-03-12 20:04:39 -0700

    test6

--- config/application.rb
+++ config/application.rb
@@ -8,9 +8,7 @@ Bundler.require(*Rails.groups)
 
 module Blogadmin
   class Application < Rails::Application
-    config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
-    config.assets.precompile += Ckeditor.assets
-    config.assets.precompile += %w(ckeditor/*)
+    
         # Settings in config/environments/* take precedence over those specified here.
     # config.assets.initialize_on_precompile = false
     # Application configuration should go into files in config/initializers
